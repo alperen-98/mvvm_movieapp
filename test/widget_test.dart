372507304ce34e1583740956e6ac5bc00dea6030
Alperen Kıldır <alperenkildir98@gmail.com>
2022-01-23 20:35:26 +0300

    test added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,47 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:movie_app/main.dart';
+import 'package:movie_app/constants/colors.dart';
+import 'package:movie_app/helpers/function_helper.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Testing FunctionHelper functions', () {
+    FunctionHelper functionHelper = FunctionHelper();
+    rateRounderTest(functionHelper);
+    borderColorTest(functionHelper);
+    dateTest(functionHelper);
   });
+
+  //testWidgets('Testing FunctionHelper class', (WidgetTester tester) async {
+
+  //});
+}
+
+void dateTest(FunctionHelper functions) {
+  String date = functions.getFormattedDate('2022-01-28');
+  expect(date, '28.1.2022');
+}
+
+void borderColorTest(FunctionHelper functions) {
+  Color color1 = functions.getRateBorderColor(1.0);
+  expect(color1, kRedColor);
+
+  Color color2 = functions.getRateBorderColor(5.5);
+  expect(color2, Colors.yellow);
+
+  Color color3 = functions.getRateBorderColor(7.2);
+  expect(color3, Colors.orange);
+
+  Color color4 = functions.getRateBorderColor(9.1);
+  expect(color4, kGreenColor);
+}
+
+void rateRounderTest(FunctionHelper functions) {
+  String result1 = functions.getRoundedRate(9.0);
+  expect(result1, '9');
+
+  String result2 = functions.getRoundedRate(1.2);
+  expect(result2, '1.2');
+
+  String result3 = functions.getRoundedRate(0.6);
+  expect(result3, '0.6');
 }
